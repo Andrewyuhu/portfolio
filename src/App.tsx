c68c9c9b40fef6c482156f64a0cd6227484b9d07
Andrew Hu <andrewyuhu02@gmail.com>
2025-04-14 16:17:25 -0700

    init project and laying out structure

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+function App() {
+  return (
+    <>
+      <header>
+        <div className="header-main-wrapper">
+          <h1>Andrew Hu</h1>
+          <span className="sub-heading">
+            Full-Stack Developer | Software Engineer
+          </span>
+        </div>
+        <nav>
+          <ul>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Experience</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <h2>About me</h2>
+        <p>
+          Hi! I'm Andrew — a passionate full-stack developer currently
+          completing my degree in Software Engineering. I enjoy building
+          intuitive user interfaces and scalable backend systems. I'm eager to
+          contribute to modern, impactful software solutions.
+        </p>
+      </section>
+      <section>
+        <h2>Skills</h2>
+        <h3>Langauges</h3>
+        <h3>Frameworks</h3>
+        <h3>Toolings</h3>
+      </section>
+      <section>
+        <h2>Projects</h2>
+      </section>
+      <section>
+        <h2>Work Experience</h2>
+      </section>
+    </>
+  );
+}
+export default App;
