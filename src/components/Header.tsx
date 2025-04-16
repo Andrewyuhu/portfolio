3a073a0404083f5958fbd5e4690fce0fe796ce06
Andrew Hu <andrewyuhu02@gmail.com>
2025-04-15 22:14:21 -0700

    added links to github & linkedin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,30 @@
+import GithubIcon from "../assets/github-icon.svg";
+import LinkedinIcon from "../assets/linkedin-icon.svg";
+
 function Header() {
   return (
     <header>
       <div className="header-main-wrapper">
         <h1>Andrew Hu</h1>
-        <span className="sub-heading">
-          Full-Stack Developer | Software Engineer
-        </span>
+        <div className="header-sub-heading">
+          Software Engineer
+          <div>
+            <a
+              href="https://github.com/Andrewyuhu"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img src={GithubIcon} className="header-icons"></img>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/andrew-hu-3a5165253/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img src={LinkedinIcon} className="header-icons"></img>
+            </a>
+          </div>
+        </div>
       </div>
     </header>
   );
